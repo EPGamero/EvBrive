5b9e94e3045f8ba56453c42409e13982518e2246
EPGamero <epg2409@gmail.com>
2022-03-08 13:23:04 -0600

    creacion de arquitectura

---
+++ Conexion/Conexion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Conexion
+{
+    public class Conexion
+    {
+        public static string ConnectionString()
+        {
+            string connectionString = "";
+            return connectionString;
+        }
+        public static SqlCommand CreateCommand(string Query, SqlConnection context)
+        {
+            SqlCommand cmd = new SqlCommand(Query, context);
+            return cmd;
+        }
+        public static int ExecuteCommand(SqlCommand cmd)
+        {
+            int rowsAffected = cmd.ExecuteNonQuery();
+            return rowsAffected;
+        }
+        public static DataTable ExecuteCommandSelect(SqlCommand cmd)
+        {
+            DataTable table = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(table);
+            return table;
+        }
+    }
+}
---
+++ Modelo/Respuesta.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Modelo
+{
+    public class Respuesta <T>
+    {
+        public int Codigo { get; set; }
+        public string Mensaje { get; set; }
+        public T Datos { get; set; }
+    }
+}
