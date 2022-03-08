b57c01aebb4e18cb723b90fea596e9fd487f635b
EPGamero <epg2409@gmail.com>
2022-03-08 13:27:26 -0600

    primer clase

---
+++ Modelo/Entidades/Clientes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo.Entidades
+{
+    public class Clientes
+    {
+        public int idCliente { get; set; }
+        public string Nombre { get; set; }
+        public string aPaterno { get; set; }
+        public string aMaterno { get; set; }
+    }
+}
